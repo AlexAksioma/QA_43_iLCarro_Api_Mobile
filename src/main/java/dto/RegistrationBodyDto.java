36b29e821ceda5a6c009dfb60d23893648a06bf0
AlexAksioma <alexmedqwerty@gmail.com>
2024-08-28 20:55:11 +0300

    added okhttp

--- src/main/java/dto/RegistrationBodyDto.java
+++ src/main/java/dto/RegistrationBodyDto.java
@@ -1,2 +1,17 @@
-package dto;public class RegistrationBodyDto {
+package dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RegistrationBodyDto {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 }
--- src/main/java/interfaces/BaseApi.java
+++ src/main/java/interfaces/BaseApi.java
@@ -1,2 +1,13 @@
-package interfaces;public interface BaseApi {
+package interfaces;
+
+import com.google.gson.Gson;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+
+public interface BaseApi {
+    String BASE_URL = "https://ilcarro-backend.herokuapp.com";
+    String REGISTRATION_URL = "/v1/user/registration/usernamepassword";
+    Gson GSON = new Gson();
+    MediaType JSON = MediaType.get("application/json");
+    OkHttpClient OK_HTTP_CLIENT = new OkHttpClient();
 }
